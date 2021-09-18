b4c14f90a5ea73f06a116de002c62f22cf05aaa1
Adi-075 <adityamhambrey323@gmail.com>
2021-09-18 17:22:48 +0530

    Add Lua LSP using lspconfig

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require ("plugins")
 require('settings')
 require('keymappings')
 require('colorscheme')
- 
+
 -- Neovim Interface Plugins --
 require('core.dashboard')
 require('core.statusline')
@@ -20,12 +20,20 @@ require('core.nv-tree')
 require('core.nv-gitsigns')
 require('core.blankline')
 require('core.discord-presence')
+require('lsp.lua-ls')
 
 -- Neovim LSP --
 require('lspauto')
 require('core.nv-compe')
 require('nv-treesitter')
 require('core.nv-lspkind')
--- require('lsp.lua-ls')
 -- require('lsp.general-ls')
 
+
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
